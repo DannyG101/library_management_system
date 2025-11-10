710ac0fff3bf33fbdb4b2ace4a8bec8d5880380a
Danny G <dannygoldberg13@gmail.com>
2025-11-10 12:10:57 +0200

    add User class

--- modules/user.py
+++ modules/user.py
@@ -1,1 +1,11 @@
-ECHO is on.
+
+
+
+class User:
+    def __init__(self, name:str, id:int, borrowed_books:list["Book"]=[]):
+        self.name = name
+        self.id = id
+        self.__borrowed_books = borrowed_books
+
+    def __str__(self):
+        return f"User name: {self.name}, User id: {self.id}, Borrowed books: {self.__borrowed_books}"
