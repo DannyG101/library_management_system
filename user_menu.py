838adc8165f22aea9ca3768aa4ae642c6c4cc522
Danny G <dannygoldberg13@gmail.com>
2025-11-10 17:47:17 +0200

    Add user_menu

--- user_menu.py
+++ user_menu.py
@@ -1,1 +1,58 @@
+class UserSystem:
 
+    def display(self):
+        my_library = Library()
+        print("Welcome to your Library\U0001F60D\n--------------------")
+        user_entry = int(input("Press 1 to continue "))
+        book_counter = 0
+        while user_entry != 1:
+            user_entry = int(input("Sorry that's not a valid choice! Press 1 to continue"))
+        print("Thank you for entering the Library system!\nPlease see the following options")
+        print("1. Add Book\n2. Add User\n3. Borrow Book\n4. Return Book"
+              "\n5. List Available Books\n6. Search Book\n7. Save & Exit")
+        user_exit = False
+        while not user_exit:
+            user_input = int(input("Please enter a number from the options above \U0001F60D "))
+            match user_input:
+              case 1:
+                book_counter += 1
+                title = input("Please enter the book title")
+                author = input("Please enter the book author")
+                book = Book(title, author, book_counter)
+                my_library.add_book(book)
+              case 2:
+                name = input("Please enter user name")
+                user_id = int(input("PLease enter id"))
+                user = User(name, user_id)
+                my_library.add_user(user)
+              case 3:
+                user_id = int(input("Please enter user id"))
+                book_isbn = int(input("Please enter book isbn"))
+                if my_library.borrow_book(user_id, book_isbn):
+                    print("Book is available. Remember to return")
+                else:
+                    print("Error: Book not found/Book is unavailable")
+              case 4:
+                user_id = int(input("Please enter user id"))
+                book_isbn = int(input("Please enter book isbn"))
+                if my_library.return_book(user_id, book_isbn):
+                    print("Thank you for returning!")
+                else:
+                    print("Error: Book not found/Book is already in Library")
+              case 5:
+                print("List of all Books available:")
+                list_of_books = my_library.list_available_books()
+                counter = 1
+                for book in list_of_books:
+                    print(f"{counter}: {book.title}")
+                    counter += 1
+              case 6:
+                title_or_author = input("Please enter the title or name of the author"
+                                        " of the book you are searching for")
+                if my_library.search_book(title_or_author):
+                    print("Book found!")
+                else:
+                    print("Book not found!")
+              case 7:
+                  print("Thanks for visiting Library")
+                  user_exit = True
