ae5d0b00ac18a2ee8da5372ba0822ac7ea798c6e
Danny G <dannygoldberg13@gmail.com>
2025-11-10 12:26:47 +0200

    add setter for borrowed_books

--- modules/user.py
+++ modules/user.py
@@ -2,10 +2,17 @@
 
 
 class User:
-    def __init__(self, name:str, id:int, borrowed_books:list["Book"]=[]):
+    def __init__(self, name:str, id:int):
         self.name = name
         self.id = id
-        self.__borrowed_books = borrowed_books
-
+        self.__borrowed_books = []
     def __str__(self):
         return f"User name: {self.name}, User id: {self.id}, Borrowed books: {self.__borrowed_books}"
+
+    @property
+    def update_borrowed_books(self):
+        return self.__borrowed_books
+
+    @update_borrowed_books.setter
+    def update_borrowed_books(self, book):
+        self.__borrowed_books.append(book)
